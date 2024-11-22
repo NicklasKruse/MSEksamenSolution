6c028a3189c248a19ef644ad828cb59a6df5c384
Nicklas Kruse <128633229+NicklasKruse@users.noreply.github.com>
2024-11-22 16:53:17 +0100

    Tilbage til class entity

--- CommonAssets/Entity.cs
+++ CommonAssets/Entity.cs
@@ -1,6 +1,6 @@
 ﻿namespace CommonAssets
 {
-    public abstract record Entity : IEquatable<Entity>
+    public abstract class Entity : IEquatable<Entity>
     {
         public Guid Id { get; init; } // init gør den immutable og readonly
 
@@ -9,23 +9,22 @@
             return HashCode.Combine(Id);
         }
 
-        #region indbygget i record
-        //public bool Equals(Entity other)
-        //{
-        //    return other?.Id == Id;
-        //}
-        //public override bool Equals(object? obj)
-        //{
-        //    return base.Equals(obj);
-        //}
+        public bool Equals(Entity other)
+        {
+            return other?.Id == Id;
+        }
+        public override bool Equals(object? obj)
+        {
+            return base.Equals(obj);
+        }
 
-        //public static bool operator !=(Entity left, Entity right)
-        //{
-        //    return EqualityComparer<Entity>.Default.Equals(left, right);
-        //}
-        //public static bool operator ==(Entity left, Entity right)
-        //{
-        //    return EqualityComparer<Entity>.Default.Equals(left, right);
-        //}
+        public static bool operator !=(Entity left, Entity right)
+        {
+            return EqualityComparer<Entity>.Default.Equals(left, right);
+        }
+        public static bool operator ==(Entity left, Entity right)
+        {
+            return EqualityComparer<Entity>.Default.Equals(left, right);
+        }
     }
 }
