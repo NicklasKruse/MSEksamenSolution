e506d41815f53a9bb0b008d1db35845fdd160643
Nicklas Kruse <128633229+NicklasKruse@users.noreply.github.com>
2024-11-29 13:06:07 +0100

    Updated NotifyActivity

--- Administration.Api/Workflows/Activities/NotifyActivity.cs
+++ Administration.Api/Workflows/Activities/NotifyActivity.cs
@@ -1,16 +1,46 @@
 ﻿using Administration.Domain.ValueObjects;
+using Dapr.Client;
 using Dapr.Workflow;
+using System.Reactive;
 
 namespace Administration.Api.Workflows.Activities
 {
-    public class NotifyActivity : WorkflowActivity<Notification, object>
+    //public class NotifyActivity : WorkflowActivity<Notification, object>
+    //{
+    //    public override async Task<object> RunAsync(WorkflowActivityContext context, Notification notification)
+    //    {
+    //        await Task.CompletedTask;
+    //        return null;
+    //    }
+    //}
+    public class NotifyActivity : WorkflowActivity<Domain.ValueObjects.Notification, Unit>
     {
-        public override async Task<object> RunAsync(WorkflowActivityContext context, Notification notification)
+        private readonly DaprClient _daprClient;
+
+        public NotifyActivity(DaprClient daprClient)
         {
-            await Task.CompletedTask;
-            return null;
+            _daprClient = daprClient;
+        }
+
+        public override async Task<Unit> RunAsync(WorkflowActivityContext context, Domain.ValueObjects.Notification notification)
+        {
+            var notificationEvent = new
+            {
+                Message = notification.Message,
+                AnimalId = notification.Animal.Id,
+                AnimalName = notification.Animal.Name,
+                Timestamp = DateTime.UtcNow
+            };
+
+            await _daprClient.PublishEventAsync(
+                "pubsub",
+                "animal-created",
+                notificationEvent);
+
+            return Unit.Default;
         }
     }
+
 }
 
 
